71e5144634f766049854b9199c5589aefa949b09
邓斌 <bin.deng@meicloud.com>
2018-05-10 16:33:37 +0800

    将Tomcat替换为Undertow

---
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/authentication/MyAccessDecisionManager.java
@@ -0,0 +1,27 @@
+package com.imooc.security.browser.authentication;
+
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.vote.AbstractAccessDecisionManager;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author DENGBIN
+ * @since 2018-5-9
+ */
+public class MyAccessDecisionManager extends AbstractAccessDecisionManager {
+
+    protected MyAccessDecisionManager(List<AccessDecisionVoter<?>> decisionVoters) {
+        super(decisionVoters);
+    }
+
+    @Override
+    public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
+        System.out.println("123");
+    }
+}
